87adfaae621c49409b96b5e6f2521eddba0384cc
Fred Tucker <fred.tucker@nbt.ag>
2019-03-20 17:36:14 +0100

    r

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="HeaderBanner">
+    <h1>FESTO</h1>
+  </div>
+
+  <div id="MenuText">
+    <p>Overview</p>
+    <p>Admin</p>
+  </div>
+
+  <div id="Stats">
+    <div id="StatsText" style="padding:10px">
+       <h3>MACHINE DATA</h3> 
+       <p>Tracking, Reporting, Storing</p>
+       <h3>FESTO CARTESIAN ROBOT</h3>
+       <p>La Rambla 82, 08002 Barcelona Spain</p>
+    </div>
+
+
+    <div style="test"> hery</div>
+  </div>
+
+  <div id="Stats2">
+    <div>
+       <button>Mileage Data </button>
+       <div></div>
+    </div>
+    <div></div>
+  </div>
+
+  
+  <div id="InfoTable">InfoTable</div>
+   <!--  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  /*   HelloWorld */
   }
 }
 </script>
@@ -22,7 +53,100 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  background-color: #FCFCFC;
 }
+
+#app div{
+  padding-top: 20px;
+}
+
+#HeaderBanner{
+  height: 30vh;
+  color:#BEC4D1;
+  font-size: 80px;
+}
+
+#MenuText{
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+}
+
+#MenuText p {
+  padding-left: 50px;
+  padding-right:50px;
+  font-size: 30px;
+}
+
+#Stats{
+  height: 50vh;
+  display: flex;
+}
+
+#Stats div{
+  width: 45%;
+  background-color: white;
+  margin: 30px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: lightblue;
+  filter: drop-shadow(3px 3px 6px lightblue);
+}
+
+#StatsText{
+  text-align: left;
+}
+
+#StatsText h3{
+  color:#BFC5D2;
+  margin-top: 50px;
+  margin-left: 50px;
+}
+
+#StatsText p{
+  color:black;
+  margin-top: 40px;
+   margin-left: 50px;
+}
+
+#Stats2{
+  height: 50vh;
+  display: flex
+}
+
+#Stats2 button{
+    background-color: #2E5AFF; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 8px;
+  width: 90%;
+}
+
+#Stats2 div{
+
+  width: 45%;
+  background-color: white;
+  margin: 30px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: lightblue;
+  filter: drop-shadow(3px 3px 6px lightblue);
+}
+
+#InfoTable{
+  background-color: yellow;
+  height: 40vh;
+}
+
+.test{
+  background-color: pink;
+}
+
+
 </style>
---
+++ src/components/CurrentLineChart.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+    <p>{{wiresharkdata}} </p>
+  </div>
+</template>
+
+<script>
+  import LineChart from './LineChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null,
+        steve: 8,
+        wiresharkdata:[0.1,0.2,8,2,4,3]
+      }
+    },
+    mounted () {
+    this.fillData()
+       
+    this.ready();
+  
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [1,2,3,4,5,6],
+          
+          datasets: [
+            {
+              label: ['Data One','d2','d3','d4'],
+              backgroundColor: 'blue',
+              data: this.wiresharkdata
+            }, 
+          ]
+        }
+      },
+    
+      getRandomInt () {
+       // return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      return this.wiresharkdata;
+      },
+      ready() {
+      var channel = this.$pusher.subscribe("last_usage");
+      channel.bind("new_value", (log) => {
+      //  this.wiresharkdata = [7,6,5,4,3,2]
+      this.wiresharkdata.shift();
+      this.wiresharkdata.push ( log.usages[0].current);
+      this.fillData();
+      });
+    }
+
+    }
+  }
+
+
+
+
+
+
+
+
+  
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
