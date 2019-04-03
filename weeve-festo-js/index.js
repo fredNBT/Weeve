31158332210a01d506b9dccae5326d2c0711646e
Fred Tucker <fred.tucker@nbt.ag>
2019-04-03 12:01:10 +0200

    working

--- weeve-festo-js/index.js
+++ weeve-festo-js/index.js
@@ -2,62 +2,66 @@ let LineChartData = [];
 let LineChartLables = [];
 
 ShowPieChart(22);
-
+let PieChartContainerSize = document.getElementById("PieChartContainer").offsetWidth;
 function ShowPieChart(Complete) {
     $('.chart').easyPieChart({
         barColor: 'blue',
-        size: '220',
+        size: (document.getElementById("PieChartContainer").offsetWidth) / 1.1,
         lineWidth: '24',
+        trackColor:'#f7f7f7',
         barColor: function (Complete) {
             var ctx = this.renderer.getCtx();
             var canvas = this.renderer.getCanvas();
             var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
             gradient.addColorStop(1, "darkblue");
             gradient.addColorStop(0, "#4fb5fe");
+          
             // gradient.addColorStop(0, "#ffe57e");
             // gradient.addColorStop(1, "#de5900");
             return gradient;
         }
     });
+
     $('.chart').data('easyPieChart').update(Complete);
 };
 
-function LoadLineChart(){
-var ctx = $('#LineChart');
-var linechart = new Chart(ctx, {
-    type:'line',
-    lineColor: "red",
-    data: {
-       labels:LineChartLables,
-       datasets:[
-           {
-            borderColor: "#80b6f4",
-            pointBorderColor: "#80b6f4",
-            pointBackgroundColor: "#80b6f4",
-            pointHoverBackgroundColor: "#80b6f4",
-            pointHoverBorderColor: "#80b6f4",
-            pointBorderWidth: 10,
-            pointHoverRadius: 10,
-            pointHoverBorderWidth: 1,
-            pointRadius: 2,
-            fill: false,
-            borderWidth: 4,
-            label:'Current Mileage',
-            data:LineChartData
-           }
-       ]
-    }
-})
+
+function LoadLineChart() {
+    var ctx = $('#LineChart');
+    var linechart = new Chart(ctx, {
+        type: 'line',
+        lineColor: "red",
+        data: {
+            labels: LineChartLables,
+            datasets: [
+                {
+                    borderColor: "#80b6f4",
+                    pointBorderColor: "#80b6f4",
+                    pointBackgroundColor: "#80b6f4",
+                    pointHoverBackgroundColor: "#80b6f4",
+                    pointHoverBorderColor: "#80b6f4",
+                    pointBorderWidth: 10,
+                    pointHoverRadius: 10,
+                    pointHoverBorderWidth: 1,
+                    pointRadius: 2,
+                    fill: false,
+                    borderWidth: 4,
+                    label: 'Current Mileage',
+                    data: LineChartData
+                }
+            ]
+        }
+    })
 
 }
 
-function test(){
+function test() {
     LineChartData.push(6);
     var today = new Date();
     LineChartLables.push(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
     LoadLineChart();
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
     LoadLineChart();
 });
--- weeve-festo-js/scripts.js
+++ weeve-festo-js/scripts.js
@@ -1,64 +1,74 @@
-$( document ).ready(function() {
-    // Smart contract variables
-    const abi = '[{"constant":false,"inputs":[{"name":"_machineID","type":"string"},{"name":"_timestamp","type":"uint256"},{"name":"_mileage","type":"uint256"}],"name":"addEntry","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_machineID","type":"string"},{"indexed":true,"name":"_timestamp","type":"uint256"},{"indexed":false,"name":"_mileage","type":"uint256"}],"name":"NewMileageEntry","type":"event"},{"constant":true,"inputs":[{"name":"_machineID","type":"string"},{"name":"_index","type":"uint256"}],"name":"getEntrieOfMachine","outputs":[{"name":"timestamp","type":"uint256"},{"name":"mileage","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_machineID","type":"string"}],"name":"getLastEntrieOfMachine","outputs":[{"name":"index","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"mileage","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
-    const contractAddress = '0x0985182941ac61f9d9fd50f0f7caa2c77a96e821';
+$(document).ready(function () {
+  // Smart contract variables
+  const abi = '[{"constant":false,"inputs":[{"name":"_machineID","type":"string"},{"name":"_timestamp","type":"uint256"},{"name":"_mileage","type":"uint256"}],"name":"addEntry","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_machineID","type":"string"},{"indexed":true,"name":"_timestamp","type":"uint256"},{"indexed":false,"name":"_mileage","type":"uint256"}],"name":"NewMileageEntry","type":"event"},{"constant":true,"inputs":[{"name":"_machineID","type":"string"},{"name":"_index","type":"uint256"}],"name":"getEntrieOfMachine","outputs":[{"name":"timestamp","type":"uint256"},{"name":"mileage","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_machineID","type":"string"}],"name":"getLastEntrieOfMachine","outputs":[{"name":"index","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"mileage","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
+  const contractAddress = '0x0985182941ac61f9d9fd50f0f7caa2c77a96e821';
 
-    // Web 3 and smart contract instance
-    const web3 = new Web3('ws://52.28.150.148:8544');
-    var MileageStorage = new web3.eth.Contract(JSON.parse(abi),contractAddress);
+  // Web 3 and smart contract instance
+  const web3 = new Web3('wss://ec2-52-28-150-148.eu-central-1.compute.amazonaws.com/ws');
+  var MileageStorage = new web3.eth.Contract(JSON.parse(abi), contractAddress);
 
-    // Mileage variables
-    var totalMileage = 0;
-    var currentMileage = 0;
+  // Mileage variables
+  var totalMileage = 0;
+  var currentMileage = 0;
 
-    // Starting value of the mileage to be counted
-    const startingMileage = 130;
+  // Starting value of the mileage to be counted
+  const startingMileage = 450;
 
-    // Initial token baöance
-    const initialTokenBalance = 200;
-    var currentTokenBalance = 200;
-    var totalCost = 0;
+  // Initial token baöance
+  const initialTokenBalance = 200;
+  var currentTokenBalance = 200;
+  var totalCost = 0;
 
-    // Exchange Rate of Token <> EUR
-    const tokenExchangeRate = 4;
-    const tokenPerMileage = 1
+  // Exchange Rate of Token <> EUR
+  const tokenExchangeRate = 4;
+  const tokenPerMileage = 1
 
-    // Machine ID
-    const machineid = "MACHINE_1";
+  // Machine ID
+  const machineid = "MACHINE_1";
 
-    // Initial retrieval of current mileage from the smart contract
-    MileageStorage.methods.getLastEntrieOfMachine(machineid).call()
-      .then((value) => { 
-       
-        totalMileage = value.mileage/1000000000;
-        currentMileage = totalMileage-startingMileage;
-        totalCost = (tokenPerMileage*currentMileage)/tokenExchangeRate;
-        currentTokenBalance = initialTokenBalance-tokenPerMileage*currentMileage;
+  // Initial retrieval of current mileage from the smart contract
+  MileageStorage.methods.getLastEntrieOfMachine(machineid).call()
+    .then((value) => {
 
-        $('.machineid').text(machineid); 
-        $('#cost_per_usage').text(tokenPerMileage);
-        $('#token_per_eur').text(tokenExchangeRate);
-        $('.mileage').text(parseFloat(currentMileage).toFixed(3) + " metres");
-        $('.token_balance').text(parseFloat(currentTokenBalance).toFixed(2)+"/"+initialTokenBalance);
-        $('.eur_balance').text(parseFloat(currentTokenBalance/tokenExchangeRate).toFixed(2)+"/"+initialTokenBalance/tokenExchangeRate);
-        $('.cost_usage').text(parseFloat(totalCost).toFixed(2));
-      });
+      totalMileage = value.mileage / 1000000000;
+      currentMileage = totalMileage - startingMileage;
+      totalCost = (tokenPerMileage * currentMileage) / tokenExchangeRate;
+      currentTokenBalance = initialTokenBalance - tokenPerMileage * currentMileage;
 
-    // Subscription to events on the smart contracts (subscribed to new mileage entries)
-    MileageStorage.events.NewMileageEntry({ fromBlock: 'latest' }, (error, event) => {
-        var difference = event.returnValues._mileage/1000000000-totalMileage;
-        totalMileage = event.returnValues._mileage/1000000000;
-        currentMileage = currentMileage+difference;
-        totalCost = totalCost + (tokenPerMileage*difference)/tokenExchangeRate;
-        currentTokenBalance = currentTokenBalance-tokenPerMileage*difference;
-        console.log(parseFloat(currentMileage).toFixed(3));
-        $('.mileage').text(parseFloat(currentMileage).toFixed(3));
-        $('.token_balance').text(parseFloat(currentTokenBalance).toFixed(2)+"/"+initialTokenBalance);
-        $('.eur_balance').text(parseFloat(currentTokenBalance/tokenExchangeRate).toFixed(2)+"/"+initialTokenBalance/tokenExchangeRate);
-        $('.cost_usage').text(parseFloat(totalCost).toFixed(2));
-        LineChartData.push(parseFloat(currentMileage).toFixed(3));
-        console.log(LineChartData);
-        LoadLineChart();
+      LineChartData.push(parseFloat(currentMileage).toFixed(3));
+      var today = new Date();
+      LineChartLables.push(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+      console.log(LineChartData)
+      LoadLineChart();
 
-      })
+      $('.machineid').text(machineid);
+      $('#cost_per_usage').text(tokenPerMileage);
+      $('#token_per_eur').text(tokenExchangeRate);
+      $('.mileage').text(parseFloat(currentMileage).toFixed(3));
+      $('.token_balance').text(parseFloat(currentTokenBalance).toFixed(2) + "/" + initialTokenBalance);
+      $('.eur_balance').text(parseFloat(currentTokenBalance / tokenExchangeRate).toFixed(2) + "/" + initialTokenBalance / tokenExchangeRate);
+      $('.cost_usage').text(parseFloat(totalCost).toFixed(2));
+    });
+
+  // Subscription to events on the smart contracts (subscribed to new mileage entries)
+  MileageStorage.events.NewMileageEntry({ fromBlock: 'latest' }, (error, event) => {
+    var difference = event.returnValues._mileage / 1000000000 - totalMileage;
+    totalMileage = event.returnValues._mileage / 1000000000;
+    currentMileage = currentMileage + difference;
+    totalCost = totalCost + (tokenPerMileage * difference) / tokenExchangeRate;
+    currentTokenBalance = currentTokenBalance - tokenPerMileage * difference;
+
+    $('.mileage').text(parseFloat(currentMileage).toFixed(3));
+    $('.token_balance').text(parseFloat(currentTokenBalance).toFixed(2) + "/" + initialTokenBalance);
+    $('.eur_balance').text(parseFloat(currentTokenBalance / tokenExchangeRate).toFixed(2) + "/" + initialTokenBalance / tokenExchangeRate);
+    $('.cost_usage').text(parseFloat(totalCost).toFixed(2));
+
+    
+    LineChartData.push(parseFloat(currentMileage).toFixed(3));
+    var today = new Date();
+    LineChartLables.push(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+    console.log(LineChartData)
+    LoadLineChart();
+
   })
+})
